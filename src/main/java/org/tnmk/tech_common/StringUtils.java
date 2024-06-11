af13b7d61085d4e56cfa7d6a92a60c84e006355e
Khoi Tran <khoi.tnm@gmail.com>
2024-06-11 12:55:43 -0400

    Add more functions to StringUtils

--- src/main/java/org/tnmk/tech_common/StringUtils.java
+++ src/main/java/org/tnmk/tech_common/StringUtils.java
@@ -1,72 +1,82 @@
 package org.tnmk.tech_common;
 
 public class StringUtils {
-  public static String[] splitByDelimiterOutsideQuote(String string, String delimiter) {
-    // Guideline: https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
-    // For example: delimiter = ","
-    // => delimiterRegExp = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-    // means: "split on the comma only if that comma has zero, or an even number of quotes ahead of it."
-    String delimiterRegExp = delimiter + "(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-    return string.split(delimiterRegExp, -1);
-  }
+    public static String[] splitByDelimiterOutsideQuote(String string, String delimiter) {
+        // Guideline: https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+        // For example: delimiter = ","
+        // => delimiterRegExp = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+        // means: "split on the comma only if that comma has zero, or an even number of quotes ahead of it."
+        String delimiterRegExp = delimiter + "(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+        return string.split(delimiterRegExp, -1);
+    }
 
-  /**
-   * @param input
-   * @return this's similar to CamelCase, but the first character is lower case.
-   */
-  public static String toFieldName(String input) {
-    String camelCase = toCamelCase(input);
-    if (isFirstTwoCharactersUppercase(camelCase)) {
-      return camelCase;
-    } else {
-      return replaceFirstCharByLowercase(camelCase);
-    }
-  }
-  public static String replaceFirstCharByLowercase(String str) {
-    if (str == null || str.isEmpty()) {
-      return str;
-    }
-    char firstChar = str.charAt(0);
-    if (Character.isLowerCase(firstChar)) {
-      return str;
-    }
-    char[] chars = str.toCharArray();
-    chars[0] = Character.toLowerCase(firstChar);
-    return new String(chars);
-  }
-
-  private static boolean isFirstTwoCharactersUppercase(String str) {
-    if (str == null || str.length() < 2) {
-      return false;
-    }
-    for (int i = 0; i < 2; i++) {
-      char c = str.charAt(i);
-      if (!Character.isUpperCase(c)) {
+    public static boolean isStartWithOneOfPrefixes(String text, String... prefixes) {
+        for (String prefix : prefixes) {
+            if (text.startsWith(prefix)) {
+                return true;
+            }
+        }
         return false;
-      }
     }
-    return true;
-  }
+
+    /**
+     * @param input
+     * @return this's similar to CamelCase, but the first character is lower case.
+     */
+    public static String toFieldName(String input) {
+        String camelCase = toCamelCase(input);
+        if (isFirstTwoCharactersUppercase(camelCase)) {
+            return camelCase;
+        } else {
+            return replaceFirstCharByLowercase(camelCase);
+        }
+    }
+
+    public static String replaceFirstCharByLowercase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        char firstChar = str.charAt(0);
+        if (Character.isLowerCase(firstChar)) {
+            return str;
+        }
+        char[] chars = str.toCharArray();
+        chars[0] = Character.toLowerCase(firstChar);
+        return new String(chars);
+    }
+
+    private static boolean isFirstTwoCharactersUppercase(String str) {
+        if (str == null || str.length() < 2) {
+            return false;
+        }
+        for (int i = 0; i < 2; i++) {
+            char c = str.charAt(i);
+            if (!Character.isUpperCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-  public static String toCamelCase(String str) {
-    if (str == null || str.isEmpty()) {
-      return str;
+    public static String toCamelCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        StringBuilder camelCase = new StringBuilder();
+        boolean capitalizeNext = false;
+        for (char c : str.toCharArray()) {
+            if (c == ' ' || c == '-' || c == '_') {
+                capitalizeNext = true;
+            } else if (capitalizeNext) {
+                camelCase.append(Character.toUpperCase(c));
+                capitalizeNext = false;
+            } else {
+                camelCase.append(c);
+            }
+        }
+        return camelCase.toString();
     }
-    StringBuilder camelCase = new StringBuilder();
-    boolean capitalizeNext = false;
-    for (char c : str.toCharArray()) {
-      if (c == ' ' || c == '-' || c == '_') {
-        capitalizeNext = true;
-      } else if (capitalizeNext) {
-        camelCase.append(Character.toUpperCase(c));
-        capitalizeNext = false;
-      } else {
-        camelCase.append(c);
-      }
-    }
-    return camelCase.toString();
-  }
 
 
 //  public static String toCamelCase(String input) {
